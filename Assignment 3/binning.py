fix mistake: u & v returned in wrong order in cross_correlate.py

Assignment 3/binning.py
@@ -49,11 +49,19 @@ def find_velocity_field(image_a, image_b):
     return u_arr, v_arr, x_pos_vel, y_pos_vel
 
 if __name__ == "__main__":
-    im_a = imread('data/LOvort_0001_a.tif')
-    im_b = imread('data/LOvort_0001_b.tif')
+    im_a = imread('data/test_a.tif')
+    im_b = imread('data/test_b.tif')
+
+    im_a = np.dot(im_a[..., :3], [0.299, 0.587, 0.114])
+    im_b = np.dot(im_b[..., :3], [0.299, 0.587, 0.114])
+
+    im_a = np.flipud(im_a)
+    im_b = np.flipud(im_b)
 
     u_arr, v_arr, x_pos_vel, y_pos_vel = find_velocity_field(im_a, im_b)
     X, Y = np.meshgrid(x_pos_vel, y_pos_vel)
-    plt.quiver(X, Y, u_arr, v_arr)
+    plt.quiver(x_pos_vel, y_pos_vel, u_arr, v_arr)
+    #plt.imshow(im_a)
+    #plt.imshow(im_b)
     plt.show()
-    print('next')
+    hi = 1

Assignment 3/cross_correlate.py
@@ -19,13 +19,16 @@ def find_pixel_shift(bin_a, bin_b, plot=False):
     fluc_b = bin_b - np.mean(bin_b)
 
     M, N = bin_a.shape
-    ov = 0.75
+    ov = 0.3
 
     max_x_shift = int((1 - ov) * N)
     max_y_shift = int((1 - ov) * M)
 
-    x_shift_arr = np.tile(np.arange(-1 * max_x_shift, max_x_shift + 1), (2 * max_y_shift + 1, 1))
-    y_shift_arr = np.tile(np.arange(-1 * max_y_shift, max_y_shift + 1), (2 * max_x_shift + 1, 1)).T
+    # x_shift_arr = np.tile(np.arange(-1 * max_x_shift, max_x_shift + 1), (2 * max_y_shift + 1, 1))
+    # y_shift_arr = np.tile(np.arange(-1 * max_y_shift, max_y_shift + 1), (2 * max_x_shift + 1, 1)).T
+
+    x_shift_arr, y_shift_arr = np.meshgrid(np.arange(-1 * max_x_shift, max_x_shift + 1),
+                                           np.arange(-1 * max_y_shift, max_y_shift + 1))
 
     R_map = np.empty(x_shift_arr.shape)
 
@@ -82,7 +85,7 @@ def find_pixel_shift(bin_a, bin_b, plot=False):
         plt.colorbar()
         plt.show()
 
-    return -1 * delta_i, -1 * delta_j
+    return -1 * delta_j, -1 * delta_i
 
 
 if __name__ == "__main__":